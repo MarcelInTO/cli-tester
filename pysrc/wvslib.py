0fe70bdbfe811943062347a07fad857b729c5069
Marcel Samek <marcel@wevr.com>
2022-11-14 19:59:43 -0800

    Add wvs specific library for non generic operations that don't belong in WCT

---
+++ pysrc/wvslib.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python3
+
+import atexit
+import gitlab
+import wct
+
+from typing import Any
+from typing import Tuple
+
+##############################################################################
+# Main test script
+##############################################################################
+
+g_gl = None
+g_tempRepos = []
+
+def init() :
+    global g_gl
+    g_gl = None
+    try:
+        g_gl =  gitlab.Gitlab(url='https://wvs.io', private_token='Sh_xiXsC1xk2yzczk8Rh')
+    except Exception:
+        print("ERROR: Could not authenticate connection to wvs.io.")
+        exit(-1)
+
+    atexit.register(_doExitCleanup)
+
+
+def _doExitCleanup() :
+    for x in g_tempRepos :
+        checkDeleteRepo(x, True)
+
+
+def _doCheckInit() :
+    global os
+    if g_gl is None :
+        print("ERROR: Use of unititialized wvs interface in test.")
+        print("       make sure to call wvslib.init() at beginning of test script")
+        exit(-1)
+
+
+def testForOK() -> str :
+    _doCheckInit()
+    return wct.xLastFullLine("OK")
+
+
+def testForERR() -> str :
+    _doCheckInit()
+    return wct.xLastFullLine("ERR")
+
+
+def checkForkRepo(name : str) -> str :
+    cloneName, err = _doForkRepo(name)
+    if err is not None:
+        wct.failTest(err)
+    return cloneName
+
+
+def checkDeleteRepo(name : str, silent=False) :
+    err = _doDeletetRepo(name)
+    if not silent and err is not None:
+        wct.failTest(err)
+
+
+def _doForkRepo(sourceRepo : str) -> Tuple[Any, str]:
+    _doCheckInit()
+
+    try:
+        sourceProject = g_gl.projects.get(sourceRepo)
+        if sourceProject is None :
+            return None, "Could not find the project to fork"
+    except Exception:
+        return None, "Exception during getting source project"
+
+
+    try:
+        forkProject = sourceProject.forks.create()
+        if forkProject is None :
+            return None, "Could not fork the project"
+    except Exception:
+        return None, "Exception during forking project"
+
+    g_tempRepos.append(forkProject.path_with_namespace)
+    return forkProject.path_with_namespace, None
+
+
+def _doDeletetRepo(repo : str) -> str :
+
+    try:
+        sourceProject = g_gl.projects.get(repo)
+        if sourceProject is None :
+            return "Delete failed. Could not find project."
+    except Exception:
+        return None, "Exception during getting source project"
+
+    try:
+        g_gl.projects.delete(sourceProject.id)
+    except Exception:
+        return None, "Exception during deleting forked project"
+
+    g_tempRepos.remove(repo)
+    return None
